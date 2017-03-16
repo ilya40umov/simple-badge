2b222be886bbac78bed5b94abecfc5f87eb7de05
Illia Sorokoumov <ilya.sorokoumov@gmail.com>
2017-03-16 12:05:37 +0100

    Create skeleton project with dependencies

---
+++ src/main/java/com/ilya40umov/badge/SimpleBadgeApplication.java
@@ -0,0 +1,17 @@
+package com.ilya40umov.badge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main entry point into the application as well as the main configuration location.
+ *
+ * @author isorokoumov
+ */
+@SpringBootApplication
+public class SimpleBadgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBadgeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ilya40umov/badge/SimpleBadgeApplicationIntegrationTests.java
@@ -0,0 +1,17 @@
+package com.ilya40umov.badge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SimpleBadgeApplicationIntegrationTests {
+
+    @Test
+    public void contextLoads() {
+        // TODO to be replaced by real integration tests
+    }
+
+}
